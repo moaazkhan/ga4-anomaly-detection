5ccb8ff571affadecf65a982153320cceed94852
Moaaz Khan <moaazkhan33@gmail.com>
2024-02-04 01:17:36 +0500

    Update bigquery-anomaly-model.sql

--- bigquery-anomaly-model.sql
+++ bigquery-anomaly-model.sql
@@ -1,10 +1,10 @@
 
 CREATE OR REPLACE MODEL `schema_name.model_name`
 OPTIONS(
-    model_type='arima_plus',
-    TIME_SERIES_DATA_COL='CT',
-    TIME_SERIES_TIMESTAMP_COL='event_date_ts',
-    TIME_SERIES_ID_COL = 'country',
+    model_type='arima_plus', -- arima plus model
+    TIME_SERIES_DATA_COL='CT', -- event count
+    TIME_SERIES_TIMESTAMP_COL='event_date_ts', --time/date
+    TIME_SERIES_ID_COL = 'country', -- select dimension column you need anomaly tection
     DECOMPOSE_TIME_SERIES=TRUE
 )
 AS (
@@ -15,7 +15,7 @@ WHERE event_name = 'first_visit'
 AND _TABLE_SUFFIX BETWEEN 
   FORMAT_DATE("%Y%m%d", DATE_SUB(CURRENT_DATE(), INTERVAL 60 DAY)) AND
   FORMAT_DATE("%Y%m%d", DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY))
-AND geo.country IN ('New Zealand','South Africa')
+AND geo.country IN ('New Zealand','South Africa') -- filter countries
 GROUP BY 1,2
 ORDER BY 2,1
 );
